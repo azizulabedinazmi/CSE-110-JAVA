78872d2596d98fef1e1a6b72bb5b07ccfba87bab
Azizul Abedin Azmi <azmi00121@gmail.com>
2023-11-19 14:02:35 +0600

    Update Student.java

--- Assignments/Assignment01/Student.java
+++ Assignments/Assignment01/Student.java
@@ -3,36 +3,43 @@ package Assignments.Assignment01;
 import java.util.Scanner;
 
 public class Student {
-    private static int studentCount = 0;
-
+    // Attributes
     private int studentId;
     private String studentName;
     private double studentGpa;
     private boolean isInternational;
     private char studentGrade;
 
-    private static final String UNIVERSITY_NAME = "My University";
-    public static String getUniversityName() {
-        return UNIVERSITY_NAME;
-    }
+    // Final Static
+    private static final String UNIVERSITY_NAME = "East West Univeersity";
     
 
+    // Access Modifier Variation
+    public static int studentCount = 0; // Public
+    private static int internationalStudentCount = 0; // Private
+    int defaultStudentCount = 0; // Default
+
+    // Constructor Overloading
     public Student() {
         this.studentId = generateStudentId();
         studentCount++;
     }
 
     public Student(String name, double gpa) {
-        this();
+        this(); // Chaining to default constructor
         this.studentName = name;
         this.studentGpa = gpa;
     }
 
     public Student(String name, double gpa, boolean isInternational) {
-        this(name, gpa);
+        this(name, gpa); // Chaining to the previous constructor
         this.isInternational = isInternational;
+        if (isInternational) {
+            internationalStudentCount++;
+        }
     }
 
+    // Accessor and Mutator methods
     public void setStudentGrade(char grade) {
         this.studentGrade = grade;
     }
@@ -41,16 +48,28 @@ public class Student {
         return studentGrade;
     }
 
-    @Override
-    public String toString() {
-        return String.format("Student{id=%d, name='%s', GPA=%.2f, international=%b, grade=%c}",
-                             studentId, studentName, studentGpa, isInternational, studentGrade);
-    }
-
+    // Static Counter Function
     public static int getStudentCount() {
         return studentCount;
     }
 
+    public static int getInternationalStudentCount() {
+        return internationalStudentCount;
+    }
+
+    // To String method
+    @Override
+    public String toString() {
+        return "Student{" +
+                "studentId=" + studentId +
+                ", studentName='" + studentName + '\'' +
+                ", studentGpa=" + studentGpa +
+                ", isInternational=" + isInternational +
+                ", studentGrade=" + studentGrade +
+                '}';
+    }
+
+    // Object Array as Attribute
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -66,16 +85,22 @@ public class Student {
         for (Student student : students) {
             System.out.println(student);
         }
-
+        System.out.println("University Name: " + UNIVERSITY_NAME);
         System.out.println("\nTotal Students: " + getStudentCount());
+        System.out.println("Total International Students: " + getInternationalStudentCount());
+       
+        
+
         scanner.close();
     }
 
     private static Student createStudent(Scanner scanner) {
         System.out.print("\nEnter student name: ");
         String name = scanner.next();
+
         System.out.print("Enter student GPA: ");
         double gpa = scanner.nextDouble();
+
         System.out.print("Is the student international? (true/false): ");
         boolean isInternational = scanner.nextBoolean();
 
@@ -85,14 +110,11 @@ public class Student {
         char grade = scanner.next().charAt(0);
         student.setStudentGrade(grade);
 
-        System.out.println("University Name: " + getUniversityName());
-        
         return student;
-        
     }
 
     private static int generateStudentId() {
+        // Assume some logic to generate a unique student ID
         return (int) (Math.random() * 1000);
     }
 }
-
